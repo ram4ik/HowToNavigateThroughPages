9a359aab217533dd3d79001cb47a6ca745da9383
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-10-21 10:05:31 +0300

    init

--- HowToNavigateThroughPages/ContentView.swift
+++ HowToNavigateThroughPages/ContentView.swift
@@ -10,12 +10,49 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        NavigationView {
+            page1().navigationBarTitle("Page1", displayMode: .inline)
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct page1: View {
+    var body: some View {
+        VStack {
+            Text("Hello From Page 1")
+                .fontWeight(.heavy)
+            
+            NavigationLink(destination: page2()) {
+                Text("Go to Page2")
+            }
+        }.navigationBarTitle("Page2", displayMode: .inline)
+    }
+}
+
+struct page2: View {
+    var body: some View {
+        VStack {
+            Text("Hello From Page 2")
+                .fontWeight(.heavy)
+            
+            NavigationLink(destination: page3()) {
+                Text("Go to Page3")
+            }
+        }.navigationBarTitle("Page3", displayMode: .inline)
+    }
+}
+
+struct page3: View {
+    var body: some View {
+        VStack {
+            Text("Hello From Page 3")
+                .fontWeight(.heavy)
+        }
     }
 }
